a6ad1e5d0808a1e869eff86318dd6528a3761b6f
felipe-cruz <piradous@gmail.com>
2022-12-04 09:59:11 -0300

    feat: POST /matches

--- app/backend/src/controllers/MatchController.ts
+++ app/backend/src/controllers/MatchController.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
+import UserService from '../services/UserService';
 import MatchService from '../services/MatchService';
 
 export default class MatchController {
   constructor(
     private matchService = new MatchService(),
+    private userService = new UserService(),
   ) {}
 
   async getAllMatches(req: Request, res: Response, next: NextFunction) {
@@ -26,13 +28,20 @@ export default class MatchController {
     }
   }
 
-//   async getIdTeam(req: Request, res: Response, next: NextFunction) {
-//     try {
-//       const { id } = req.params;
-//       const team = await this.teamService.getIdTeam(Number(id));
-//       return res.status(200).json(team);
-//     } catch (error) {
-//       next(error);
-//     }
-//   }
+  async changeMatches(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { authorization } = req.headers;
+      const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = req.body;
+      await this.userService.validateLogin(authorization);
+      const match = await this.matchService.changeMatches(
+        homeTeam,
+        awayTeam,
+        homeTeamGoals,
+        awayTeamGoals,
+      );
+      return res.status(201).json(match);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,6 +1,6 @@
 import TeamsModel from '../database/models/TeamsModel';
 import MatchesModel from '../database/models/MatchesModel';
-// import ValidationError from '../errors/ValidationError';
+import ValidationError from '../errors/ValidationError';
 // import IMatch from '../interfaces/IMatch';
 
 export default class MatchService {
@@ -37,10 +37,36 @@ export default class MatchService {
     return matches;
   };
 
-  // getIdTeam = async (id: number): Promise<ITeam | null> => {
-  //   const team = await TeamsModel.findByPk(id);
+  validateMatches = async (
+    homeTeam: number,
+    awayTeam: number,
+  ) => {
+    if (homeTeam === awayTeam) {
+      throw new
+      ValidationError(422, 'It is not possible to create a match with two equal teams');
+    }
+    const findHomeTeam = await TeamsModel.findByPk(homeTeam);
+    const findAwayTeam = await TeamsModel.findByPk(awayTeam);
+    if (!findHomeTeam || !findAwayTeam) {
+      throw new ValidationError(404, 'There is no team with such id!');
+    }
+  };
 
-  //   if (!team) throw new ValidationError(400, 'Team not found');
-  //   return team;
-  // };
+  changeMatches = async (
+    homeTeam: number,
+    awayTeam: number,
+    homeTeamGoals: number,
+    awayTeamGoals: number,
+  ) => {
+    await this.validateMatches(homeTeam, awayTeam);
+    const insertMatch = await MatchesModel.create({
+      homeTeam,
+      awayTeam,
+      homeTeamGoals,
+      awayTeamGoals,
+      inProgress: 1,
+    });
+    console.log(insertMatch);
+    return insertMatch;
+  };
 }
